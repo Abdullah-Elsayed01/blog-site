e8784a758c5ea79e205f113d1c12fe14a40cd0d1
Abdullah <abdullahmasoud.elsayed@gmail.com>
2025-06-18 16:20:28 +0300

    v.1.0.1

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,22 +1,39 @@
 import Post from '../post/Post';
-import articles from '../../../articles.json';
+import articlesData from '../../../articles.json';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useDebounced } from "../hooks/useDebounced";
 export default function Posts({searchValue}) {
   const debouncedValue = useDebounced(searchValue);
   const navigate = useNavigate()
-  const [originalArticlesTotal, setOriginalArticlesTotal] = useState(articles)
-  const [articlesTotal, setArticlesTotal] = useState(articles)
-  articlesTotal.sort((a,b) => b.created_at.localeCompare(a.created_at)) // recent posts
+  const [originalArticles, setOriginalArticles] = useState([])
+  const [articlesDisplayed, setArticlesDisplayed] = useState([])
+
   useEffect(() => {
-    const filteredArticles = originalArticlesTotal.filter((article) => article.title.toLowerCase().includes(debouncedValue.toLowerCase()))
-    setArticlesTotal(filteredArticles)
-  }, [debouncedValue, originalArticlesTotal])
+    const sortedInitialData = articlesData.sort((a, b) => {
+      const dataA = a.created_at || '';
+      const dataB = b.created_at || '';
+      // sort recent posts
+      return dataB.localeCompare(dataA);
+    })
+    setOriginalArticles(sortedInitialData);
+    setArticlesDisplayed(sortedInitialData);
+  }, [])
+  useEffect(() => {
+    if (debouncedValue) {
+      const filteredData = originalArticles.filter((article) => {
+        return article.title.toLowerCase().includes(debouncedValue.toLowerCase())
+      })
+      console.log(filteredData)
+      setArticlesDisplayed(filteredData)
+    } else {
+      setArticlesDisplayed(originalArticles)
+    }
+  }, [debouncedValue, originalArticles])
   return(
     <section className='posts-section container'>
       <div className='grid auto-rows-min gap-2'>
-        {articlesTotal.map((article) => {
+        {articlesDisplayed.map((article) => {
           return <Post onclick={() => navigate(`/blogs/${article.id}`)} key={article.id} content={article}/>
         })}
       </div>
